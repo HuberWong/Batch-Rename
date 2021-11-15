65b2ced70062dfe3aeb5702368f1b955f43a3fac
HuberWong <huberwong@163.com>
2021-11-15 15:40:41 +0800

    实现了基本功能

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # Batch-rename
  批量重命名腾讯问卷文件
+
+`./source/config.json` 文件为配置文件，在这里改配置就行
+
+## 配置参数说明
+
+`./source/config.json` 文件为配置文件，在这里改配置就行
+
+1. `csvPath`：从腾讯问卷导出的 `.csv` 文件位置
+2. `needRename`：从腾讯问卷批量导出的文件
+3. `renamed`：重命名之后的文件
+4. `separator`：分隔符
+5. `subname`：新文件名单个部分列表。A 列为 1，L 为 11，M 为 12，一次类推
+
+## 发行版
+
+`./release` 目录下 `renamer.exe` 为发行版，不需要 python 环境，双击运行即可
+
---
+++ source/renamer.py
@@ -0,0 +1,54 @@
+import os
+
+
+config = eval(open("./config.json","r").read())
+csv = open(config['csvPath'],'r',encoding='utf-8').read()
+csvList = csv.split('\n')
+workList = []
+oldDocList = []
+newDocList = []
+docFlag = 0
+workSpace = os.getcwd()
+# print(workSpace)
+
+# 制作旧文件名列表
+# csv 文件切割
+for i in range(len(csvList)):
+    csvList[i] = csvList[i].split(',')
+# 确定 docFlag
+for i in range(len(csvList[2]) - 1):
+    if 'https://wj.qq.com/api/files/download' in csvList[2][i]:
+        docFlag = i
+        break
+
+for i in range(1,len(csvList) - 1):
+    oldDocList.append(csvList[i][13].split("，")[1].strip('")'))
+# print(oldDocList)
+
+
+# 制作新文件名列表
+for i in range(len(oldDocList)):
+    newDocList.append('')
+
+
+for subname in config['subname']:
+    if  not (len(newDocList[0]) == 0) :
+        for i in range(len(newDocList)):
+            newDocList[i] += config['separator'] 
+    for i in range(len(newDocList)):
+        newDocList[i] += csvList[i+1][subname].strip('"\t')
+for i in range(len(oldDocList)):
+    newDocList[i] += "." + oldDocList[0].split(".")[1]
+    
+# print(oldDocList)
+# print(newDocList)
+for i in range(len(oldDocList)):
+    oldDocList[i] = workSpace + "\\" + config['needRename'] + oldDocList[i]
+    newDocList[i] = workSpace + "\\" + config['renamed'] + newDocList[i]
+
+for i in range(len(oldDocList)):
+    os.system("copy "+oldDocList[i]+" "+newDocList[i])
+print(newDocList)
+
+
+print(oldDocList[0].split(".")[1])
